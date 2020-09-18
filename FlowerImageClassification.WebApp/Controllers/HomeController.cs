058fe37b9398e13f160f5223e2c343ff3cbe1a86
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-18 15:04:36 +0700

    Update files

--- FlowerImageClassification.WebApp/Controllers/HomeController.cs
+++ FlowerImageClassification.WebApp/Controllers/HomeController.cs
@@ -1,16 +1,34 @@
-﻿using FlowerImageClassification.WebApp.Models;
+﻿using FlowerImageClassification.Shared;
+using FlowerImageClassification.Shared.Image;
+using FlowerImageClassification.WebApp.Models;
+using FlowerImageClassification.Shared.Models.ImageHelpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace FlowerImageClassification.WebApp.Controllers
 {
+	[ApiController]
 	public class HomeController : Controller
 	{
+		public IConfiguration configuration { get; }
+
+		private readonly PredictionEnginePool<ImageDataInMemory, ImagePrediction> predictionEnginePool;
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(PredictionEnginePool<ImageDataInMemory, ImagePrediction> predictionEnginePool, IConfiguration configuration, ILogger<HomeController> logger)
 		{
+			// Get the ML Model Engine injected, for scoring
+			this.predictionEnginePool = predictionEnginePool;
+			this.configuration = configuration;
+			// Get other injected dependencies
 			_logger = logger;
 		}
 
@@ -29,5 +47,55 @@ namespace FlowerImageClassification.WebApp.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		[HttpGet]
+		[Route("api/ClassifyImage")]
+		public ActionResult<string> Get()
+		=> "Hello World";
+
+		[HttpPost]
+		[Route("api/ClassifyImage")]
+		public async Task<IActionResult> ClassifyImage(IFormFile imageFile)
+		{
+			if (imageFile.Length == 0)
+				return BadRequest();
+			var memoryStream = new MemoryStream();
+
+			// Asynchronously copies the content of the uploaded file
+			await imageFile.CopyToAsync(memoryStream);
+
+			// Check that the image is valid
+			byte[] imageData = memoryStream.ToArray();
+			if (imageData.IsValidImage())
+				return StatusCode(StatusCodes.Status415UnsupportedMediaType);
+			_logger.LogInformation("Start processing image...");
+
+			// Measure execution time
+			var watch = Stopwatch.StartNew();
+
+			// Set the specific image data into the ImageInputData type used in the DataView
+			var imageInputData = new ImageDataInMemory(imageData, null, null);
+
+			// Predict code for provided image
+			var prediction = predictionEnginePool.Predict(imageInputData);
+
+			// Stop measuring time
+			watch.Stop();
+			var elapsedTime = watch.ElapsedMilliseconds;
+			_logger.LogInformation($"Image processed in {elapsedTime} miliseconds");
+
+			// Predict the image's label with highest probability
+			var bestPrediction = new ImagePredictedLabelWithProbability
+			{
+				PredictedLabel = prediction.PredictedLabel,
+				Probability = prediction.Score.Max(),
+				PredictionExecutionTime = elapsedTime,
+				ImageID = imageFile.FileName
+			};
+			return Ok(bestPrediction);
+		}
+
+		private string getAbsolutePath(string relativePath)
+			=> FileUtils.GetAbsolutePath(typeof(Program).Assembly, relativePath);
 	}
 }
--- FlowerImageClassification.WebApp/Startup.cs
+++ FlowerImageClassification.WebApp/Startup.cs
@@ -1,8 +1,14 @@
+using FlowerImageClassification.Shared.Image;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ML;
+using System;
+using System.Drawing;
+using System.IO;
 
 namespace FlowerImageClassification.WebApp
 {
@@ -19,6 +25,12 @@ namespace FlowerImageClassification.WebApp
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllersWithViews();
+
+			// Register the PredictionEnginePool as a service in the IoC container for DI.
+			services.AddPredictionEnginePool<ImageDataInMemory, ImagePrediction>().
+				FromFile(Configuration["MLModel:MLModelFilePath"]);
+
+			// WarmUpPredictionEnginePool(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +56,42 @@ namespace FlowerImageClassification.WebApp
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
 			});
+		}
+
+		public static void WarmUpPredictionEnginePool(IServiceCollection services)
+		{
+			//#1 - Simply get a Prediction Engine
+			var predictionEnginePool = services.BuildServiceProvider().GetRequiredService<PredictionEnginePool<ImageDataInMemory, ImagePrediction>>();
+			var predictionEngine = predictionEnginePool.GetPredictionEngine();
+			predictionEnginePool.ReturnPredictionEngine(predictionEngine);
+
+			// #2 - Predict
+			// Get a sample image
+
+			Image img = Image.FromFile(@"...");
+			byte[] imageData;
+			IFormFile imageFile;
+			using (MemoryStream ms = new MemoryStream())
+			{
+				img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+				//To byte[] (#1)
+				imageData = ms.ToArray();
+
+				//To FormFile (#2)
+				imageFile = new FormFile(ms, 0, ms.Length, "BlackRose", "BlackRose.png");
+			}
+
+			var imageInputData = new ImageDataInMemory(imageBytes: imageData, label: null, imagePath: null);
+
+			// Measure execution time.
+			var watch = System.Diagnostics.Stopwatch.StartNew();
+
+			var prediction = predictionEnginePool.Predict(imageInputData);
+
+			// Stop measuring time.
+			watch.Stop();
+			var elapsedMs = watch.ElapsedMilliseconds;
+
 		}
 	}
 }
