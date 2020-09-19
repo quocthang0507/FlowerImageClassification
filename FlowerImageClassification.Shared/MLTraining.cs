16839bee9e45865f98b30966c7f75a8a34cdd1fd
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-19 07:15:02 +0700

    Update files

--- FlowerImageClassification.Shared/MLTraining.cs
+++ FlowerImageClassification.Shared/MLTraining.cs
@@ -207,7 +207,7 @@ namespace FlowerImageClassification.Shared
 				// The feature column name should has same name in ImageDataInMemory
 				FeatureColumnName = "ImageBytes",
 				// Change the architecture to different DNN architecture
-				Arch = ImageClassificationTrainer.Architecture.ResnetV250,
+				Arch = ImageClassificationTrainer.Architecture.MobilenetV2,
 				// Number of training iterations
 				Epoch = 200,
 				// Number of samples to use for mini-batch training
