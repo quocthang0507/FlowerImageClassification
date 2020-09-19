6598e9204fe07da084ac6839929fe97386670bdc
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-19 10:47:28 +0700

    Upload files

--- FlowerImageClassification.WebApp/wwwroot/js/site.js
+++ FlowerImageClassification.WebApp/wwwroot/js/site.js
@@ -43,3 +43,12 @@ $(document).ready(function () {
 		}
 	});
 });
+
+/////////////// File upload form ///////////////
+$(document).ready(function () {
+	// Add the following code if you want the name of the file appear on select
+	$(".custom-file-input").on("change", function () {
+		var fileName = $(this).val().split("\\").pop();
+		$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+	});
+});
