4451cbcbf6e7f4a68dc04ce1ae8f967ee1fd5bcd
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-10-15 21:41:03 +0700

    Update prediction project

--- FlowerImageClassification.Prediction/Program.cs
+++ FlowerImageClassification.Prediction/Program.cs
@@ -8,23 +8,20 @@ namespace FlowerImageClassification.Prediction
 	class Program
 	{
 		static string outputMlNetModelFilePath, imagesFolderPathForPredictions, fullImagesetFolderPath, consoleOutputPath;
-		static string[] trainedModels = { "imageClassifier_inceptionv3.zip", "imageClassifier_mobilenetv2.zip", "imageClassifier_resnetv250.zip", "imageClassifier_resnetv2101.zip" };
 
 		static void Main(string[] args)
 		{
 			// Redirect console output to a stream
 			SetPaths();
-			foreach (var model in trainedModels)
+			string model = ""; // Must put the name of trained model file
+			string output = Path.Combine(consoleOutputPath, $"EvaluatingOutput_{model}_{DateTime.Now.ToString("d_M_y h_m_s")}.txt");
+			outputMlNetModelFilePath = Path.Combine(outputMlNetModelFilePath, model);
+			using (MirrorOutput capturing = new MirrorOutput(output))
 			{
-				string output = Path.Combine(consoleOutputPath, $"EvaluatingOutput_{model}_{DateTime.Now.ToString("d_M_y h_m_s")}.txt");
-				outputMlNetModelFilePath = Path.Combine(outputMlNetModelFilePath, model);
-				using (MirrorOutput capturing = new MirrorOutput(output))
-				{
-					MLTraining mlTraining = new MLTraining(outputMlNetModelFilePath, imagesFolderPathForPredictions, fullImagesetFolderPath);
-					//mlTraining.EvaluateModel();
-					mlTraining.TrySinglePrediction();
-					//mlTraining.TryMultiplePredictions();
-				}
+				MLTraining mlTraining = new MLTraining(outputMlNetModelFilePath, imagesFolderPathForPredictions, fullImagesetFolderPath);
+				//mlTraining.EvaluateModel();
+				mlTraining.TrySinglePrediction();
+				//mlTraining.TryMultiplePredictions();
 			}
 			// End the pipeline and write to file
 			Console.WriteLine("Press any key to exit...");
