18bcc4967a00737232944dfd0c3844eb4f434652
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-10-29 16:14:54 +0700

    Update files

--- FlowerImageClassification.WebApp/Controllers/AdminController.cs
+++ FlowerImageClassification.WebApp/Controllers/AdminController.cs
@@ -18,8 +18,8 @@ namespace FlowerImageClassification.WebApp.Controllers
 			this.flowerService = flowerService;
 			FlowerDataset imageset = new FlowerDataset((LiteDbFlowerService)flowerService);
 			//imageset.InitializeImageSet();
-			//imageset.SaveToFile(@"D:\GitHub\FlowerImageClassification\FlowerImageClassification.WebApp\Backup\");
-			//imageset.RestoreImageSet(@"D:\GitHub\FlowerImageClassification\FlowerImageClassification.WebApp\Backup\");
+			//imageset.SaveToFile(@"D:\GitHub\FlowerImageClassification\FlowerImageClassification.WebApp\BackupFiles\");
+			//imageset.RestoreImageSet(@"D:\GitHub\FlowerImageClassification\FlowerImageClassification.WebApp\BackupFiles\");
 		}
 
 		[BasicAuthentication]
